579be4fea72721f63ec788ed0623181acc365131
colburncodes <colburnsanders@gmail.com>
2023-06-27 15:52:51 -0500

    feat: :construction: updated register api to generate db with model

--- my-next-project/src/app/api/users/register/route.ts
+++ my-next-project/src/app/api/users/register/route.ts
@@ -1,10 +1,31 @@
 // https://localhost:3000/api/users/register
 import { connectDB } from "@/config/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
+import { User } from "../../../models/user";
+import bcrypt from "bcrypt";
+
 connectDB();
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({
-    message: "users/register api accessed with POST method",
-  });
+  try {
+    const request = await req.json();
+    const { name, email, password } = request;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error("User already exist!");
+    }
+
+    return bcrypt.hash(password, 10).then((hash) =>
+      User.create({ name, email, password: hash }).then((user) => {
+        return NextResponse.json(
+          { message: "User created successfully" },
+          { status: 201 }
+        );
+      })
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message, status: 500 });
+  }
 }
